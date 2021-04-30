e2ccaf8ace8fb3bf4c2359df50c2fa4f338a0a41
Ethan <ethan.hawk@valpo.edu>
2021-04-30 15:44:14 -0500

    Updated test_runner.c with a new test on ecrypting a message

--- test_runner.c
+++ test_runner.c
@@ -15,8 +15,17 @@ test_reverse_string(const MunitParameter params[], void* data) {
 
 static MunitResult
 test_vigenere(const MunitParameter params[], void* data) {
+    char *key_1 = "KEY";
+    char *key_2 = "LONGERKEY";
+    char *plain_message_1 = "SHORTMESSAGE";
+    char *crypt_key_1_message_1 = "CLMBXKOWQKKC";
+    char *plain_message_2 = "MUCHMUCHLONGERMESSAGE";
+
     char *string_1 = "abcdefghijklmnopqrstuvwxyz";
     munit_assert_int(0, ==, getIndexOfChar(string_1, 'a'));
+    munit_assert_int(25, ==, getIndexOfChar(string_1, 'z'));
+
+    munit_assert_string_equal(translate_string(key_1, plain_message_1, 'e'), crypt_key_1_message_1);
 
     return MUNIT_OK;
 }
@@ -24,7 +33,7 @@ test_vigenere(const MunitParameter params[], void* data) {
 static MunitTest
 test_suite_tests[] = {
     { (char*) "Reverse String Header", test_reverse_string, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
-    { (char*) "Vigenere Header", test_vigenere, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
+    { (char*) "Vigenere Header",             test_vigenere, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
 };
 
 
